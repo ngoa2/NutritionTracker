ed9b71b9a24fecbc62854559dc7a465ac73b6a47
Chris Yee <chris-yee@Chriss-MacBook-Pro.local>
2020-07-12 00:32:04 -0700

    Prototype 2.0

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList} from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
+  
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...courseGoals, { id: Math.random().toString(), value: enteredGoal}]);
+  };
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer} onChangeText={}>
+      <View style={styles.inputContainer}>
         <TextInput 
           placeholder="Course Goal" 
           style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View />    
+      <FlatList
+        keyExtractor={(item, index) => item.id} 
+        data={courseGoals} 
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+        />
     </View>
   );
 }
@@ -31,5 +51,13 @@ const styles = StyleSheet.create({
     width:'80%', 
     borderColor: 'black', 
     borderWidth: 1, 
-    padding: 10}
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black', 
+    borderWidth: 1
+  }
 }); 
