08bc7eff5b881182330703f1f57d1db2660ac4dd
Chris Yee <chris-yee@Chriss-MacBook-Pro.local>
2020-07-21 20:55:03 -0700

    Searchbar addition

--- screens/AchievementScreen.js
+++ screens/AchievementScreen.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const AchievementScreen = () => {
   return (
     <View style={styles.container}>
+      <SearchBar />
       <Text>Achievement</Text>
     </View>
   );
