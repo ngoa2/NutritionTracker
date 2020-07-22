be466ca55a25c7a619e610f137eaa67346eb9ef9
tiphanieleung <leungti@uw.edu>
2020-07-22 01:31:31 -0700

    create basic full layout for achievement screen

--- screens/AchievementScreen.js
+++ screens/AchievementScreen.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
 import SearchBar from "../components/SearchBar";
-import { Card, CardItem, Container } from "native-base";
+import { Card, CardItem, Container, Body } from "native-base";
+import DashboardCard from "../components/DashboardCard";
 
 const AchievementScreen = () => {
   return (
     <Container>
       <View style={styles.background}>
 
-      <View style={styles.searchBar}>
+      <View style={styles.topHalf}>
+        <View style={styles.searchBar}>
         <SearchBar />
+        </View>
         <Text style={styles.textTitle}>Chicken</Text>
+        <View style={styles.test2}>
+        <Body style={styles.roundedBody}>
+            <DashboardCard calories="10" meal="dinner" mealTime="10pm" />
+            <DashboardCard calories="10" meal="dinner" mealTime="10pm" />
+            <DashboardCard calories="10" meal="dinner" mealTime="10pm" />
+        
+      </Body>
+      </View>
       </View>
 
       
+
+
 
     </View>
     </Container>
@@ -32,26 +45,48 @@ const styles = StyleSheet.create({
     backgroundColor: "#2CBA8D",
   },
 
-  searchBar: {
+  topHalf: {
     flex: 1,
     width: '100%',
+    height: '100%',
     flexDirection: "column",
     justifyContent: "flex-start",
     alignItems: "center",
     position: 'absolute',
-    padding: 25,
+    // padding: 25,
+    // marginTop: 80,
+  },
+
+  searchBar: {
+    position: 'relative',
+    width: '100%',
+    paddingHorizontal: 25,
     marginTop: 80,
   },
 
   textTitle: {
     width: '100%',
+    padding: 25,
     fontSize: 34,
     fontStyle: "normal",
     fontWeight: "bold",
     color: "white",
     position: "relative",
-    top: 20,
+    top: 0,
     textAlign: 'left',
+  },
+
+  roundedBody: {
+    backgroundColor: "#F9F9F9",
+    paddingTop: 8,
+    borderRadius: 36,
+  },
+
+  test2: {
+    position: 'relative',
+    top: 10,
+    flexDirection: "column",
+    backgroundColor: "#2CBA8D",
   },
 });
 export default AchievementScreen;
