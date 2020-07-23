419ef9f8e88eb8f46164a1aa989f82cec70d9d00
Chris Yee <chris-yee@Chriss-MacBook-Pro.local>
2020-07-22 21:15:17 -0700

    Update

--- screens/AchievementScreen.js
+++ screens/AchievementScreen.js
@@ -1,47 +1,36 @@
 import React from "react";
-<<<<<<< HEAD
-import { StyleSheet, Text, View } from "react-native";
-=======
-import { StyleSheet, Text, View, Dimensions, FlatList, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  FlatList,
+  ScrollView,
+} from "react-native";
 import SearchBar from "../components/SearchBar";
 import { Card, CardItem, Container, Body } from "native-base";
 import AchievementCard from "../components/AchievementCard";
->>>>>>> 5a2b5d1d12fa50fb4ac4915a11dc0b697fd2d87a
 
 const AchievementScreen = () => {
-
-  let DATA = [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}]
+  let DATA = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }];
 
   return (
-<<<<<<< HEAD
-    <View style={styles.container}>
-      <Text>Achievement</Text>
-=======
     <Container>
       <View style={styles.background}>
-
-      <View style={styles.topHalf}>
-        <View style={styles.searchBar}>
-        <SearchBar />
+        <View style={styles.topHalf}>
+          <View style={styles.searchBar}>
+            <SearchBar />
+          </View>
+          <Text style={styles.textTitle}>Chicken</Text>
+          <View style={styles.test2}>
+            <Body style={styles.roundedBody}>
+              <AchievementCard />
+              <AchievementCard />
+              <AchievementCard />
+            </Body>
+          </View>
         </View>
-        <Text style={styles.textTitle}>Chicken</Text>
-        <View style={styles.test2}>
-        <Body style={styles.roundedBody}>
-            
-        <AchievementCard />
-        <AchievementCard />
-        <AchievementCard />
-        
-      </Body>
       </View>
-      </View>
-
-      
-
-
-
->>>>>>> 5a2b5d1d12fa50fb4ac4915a11dc0b697fd2d87a
-    </View>
     </Container>
   );
 };
@@ -60,25 +49,25 @@ const styles = StyleSheet.create({
 
   topHalf: {
     flex: 1,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     flexDirection: "column",
     justifyContent: "flex-start",
     alignItems: "center",
-    position: 'absolute',
+    position: "absolute",
     // padding: 25,
     // marginTop: 80,
   },
 
   searchBar: {
-    position: 'relative',
-    width: '100%',
+    position: "relative",
+    width: "100%",
     paddingHorizontal: 25,
     marginTop: 80,
   },
 
   textTitle: {
-    width: '100%',
+    width: "100%",
     padding: 25,
     fontSize: 34,
     fontStyle: "normal",
@@ -86,25 +75,24 @@ const styles = StyleSheet.create({
     color: "white",
     position: "relative",
     top: 0,
-    textAlign: 'left',
+    textAlign: "left",
   },
 
   roundedBody: {
     backgroundColor: "#F9F9F9",
     paddingTop: 20,
     borderRadius: 36,
-    width: '100%',
+    width: "100%",
     // overflow: "scroll",
   },
 
   test2: {
-    position: 'relative',
+    position: "relative",
     top: 10,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     flexDirection: "column",
     backgroundColor: "#2CBA8D",
   },
-
 });
 export default AchievementScreen;
