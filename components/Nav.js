97a53592e1da13f57a614762209fa53ea531e9c1
cyee20-1724226 <cyee20@uw.edu>
2020-09-03 01:30:02 -0700

    New Nav Bar

--- components/Nav.js
+++ components/Nav.js
@@ -4,186 +4,87 @@ import {
   Image,
   StyleSheet,
   TouchableOpacity,
-  TouchableWithoutFeedback,
-  Animated,
   Text,
   Alert,
 } from "react-native";
-import { NavigationEvents } from "react-navigation";
+import OverviewScreen from "../screens/OverviewScreen";
+// import AchievementScreen from "../screens/AchievementCard";
+import ProfileScreen from "../screens/ProfileScreen";
+import DashboardScreen from "../screens/DashboardScreen";
+import SearchScreen from "../screens/SearchScreen";
 
 export default class BottomNavigator extends Component {
-  toggleOpen = () => {};
+  // toggleOpen = () => {};
 
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "column",
-          backgroundColor: "grey",
-        }}
-      >
-        <View
-          style={{
-            position: "absolute",
-            alignSelf: "center",
-            backgroundColor: "grey",
-            width: 70,
-            height: 70,
-            borderRadius: 35,
-            bottom: 35,
-            zIndex: 10,
-          }}
-        >
-          <TouchableWithoutFeedback onPress={this.toggleOpen}>
-            <View style={[styles.button, styles.actionBtn]}>
-              <Image
-                style={{ width: 60, height: 60 }}
-                resizeMode="contain"
-                // source={{
-                //   uri:
-                //     "https://icon-library.net/images/android-plus-icon/android-plus-icon-0.jpg",
-                // }}
-              />
-            </View>
-          </TouchableWithoutFeedback>
-        </View>
-        <View
-          style={{
-            borderRadius: 10,
-
-            position: "absolute",
-            backgroundColor: "white",
-            border: 2,
-            radius: 3,
-            shadowOpacity: 0.3,
-            shadowRadius: 3,
-            shadowOffset: {
-              height: 3,
-              width: 3,
-            },
-            x: 0,
-            y: 0,
-            style: { marginVertical: 5 },
-            bottom: 0,
-            width: "95%",
-            height: 70,
-            flexDirection: "row",
-            justifyContent: "space-between",
-            paddingVertical: 10,
-            paddingHorizontal: 25,
-          }}
-        >
-          <View
-            style={{
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
+      <View style={styles.navStyle}>
+        {/* Add */}
+        <View style={styles.addButton}>
+          <TouchableOpacity
+            onPress={() => {
+              Alert.alert("click");
             }}
           >
+            <Image source={require("../src/images/icons/add_icon.png")}></Image>
+          </TouchableOpacity>
+        </View>
+
+        {/* Bottom Navigation*/}
+        <View style={styles.bottomNav}>
+          {/* Home */}
+          <View style={styles.category}>
             <TouchableOpacity
               onPress={() => {
                 Alert.alert("click");
               }}
             >
               <Image
-                style={{ width: 30, height: 30 }}
-                source={{
-                  uri:
-                    "http://pluspng.com/img-png/home-icon-png-home-house-icon-image-202-512.png",
-                }}
-                onPress={() => {
-                  Alert.alert("");
-                }}
+                style={{ marginLeft: 15 }}
+                source={require("../src/images/icons/home_icon.png")}
               ></Image>
             </TouchableOpacity>
-            <Text style={{ justifyContent: "center", alignItems: "center" }}>
-              Home
-            </Text>
           </View>
 
-          <View
-            style={{
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              marginStart: 30,
-            }}
-          >
+          {/* Overview */}
+          <View style={styles.category}>
             <TouchableOpacity
               onPress={() => {
                 Alert.alert("click");
               }}
             >
               <Image
-                style={{ width: 30, height: 30 }}
-                source={{
-                  uri:
-                    "http://simpleicon.com/wp-content/uploads/active-search.png",
-                }}
-                onPress={() => {
-                  Alert.alert("click");
-                }}
-              />
+                style={{ marginRight: 35 }}
+                source={require("../src/images/icons/overview_icon.png")}
+              ></Image>
             </TouchableOpacity>
-            <Text style={{ justifyContent: "center", alignItems: "center" }}>
-              search{" "}
-            </Text>
+          </View>
+          {/* Achievment */}
+          <View style={styles.category}>
+            <TouchableOpacity
+              onPress={() => {
+                Alert.alert("click");
+              }}
+            >
+              <Image
+                style={{ marginLeft: 35 }}
+                source={require("../src/images/icons/achievements_icon.png")}
+              ></Image>
+            </TouchableOpacity>
           </View>
 
-          <View
-            style={{
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "space-between",
-              marginStart: 85,
-            }}
-          >
+          {/* Profile */}
+          <View style={styles.category}>
             <TouchableOpacity
               onPress={() => {
                 Alert.alert("click");
               }}
             >
               <Image
-                source={{
-                  uri:
-                    "http://pixsector.com/cache/a1dd5a90/av895b2bd52a42e99ee3c.png",
-                }}
-                onPress={() => {
-                  Alert.alert("click");
-                }}
-                style={{ marginHorizontal: 16, width: 30, height: 30 }}
-                containerStyle={{ marginHorizontal: 16 }}
-              />
+                style={{ marginRight: 15 }}
+                source={require("../src/images/icons/profile_icon.png")}
+              ></Image>
             </TouchableOpacity>
-            <Text style={{ justifyContent: "center", alignItems: "center" }}>
-              Menu{" "}
-            </Text>
-          </View>
-          <View
-            style={{
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "flex-end",
-            }}
-          >
-            <TouchableOpacity
-              onPress={() => {
-                Alert.alert("click");
-              }}
-            >
-              <Image
-                source={{
-                  uri:
-                    "https://serfob.s3.amazonaws.com/media/settings-icon-png82e-4c02-9f9a-51398c8713ae.png",
-                }}
-                style={{ marginHorizontal: 16, width: 30, height: 30 }}
-                containerStyle={{ marginHorizontal: 16 }}
-              />
-            </TouchableOpacity>
-            <Text style={{ justifyContent: "center", alignItems: "center" }}>
-              Setting{" "}
-            </Text>
           </View>
         </View>
       </View>
@@ -192,13 +93,30 @@ export default class BottomNavigator extends Component {
 }
 
 const styles = StyleSheet.create({
-  MainContainer: {
+  navStyle: {
     flex: 1,
-    justifyContent: "center",
+    flexDirection: "column",
+    backgroundColor: "#E5E5E5",
     alignItems: "center",
-    backgroundColor: "blue",
   },
-  button: {
+  category: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bottomNav: {
+    borderRadius: 10,
+    position: "absolute",
+    backgroundColor: "white",
+    bottom: "3%",
+    height: 62,
+    width: "95%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+  },
+  add: {
     width: 60,
     height: 60,
     alignItems: "center",
@@ -214,48 +132,16 @@ const styles = StyleSheet.create({
     top: 5,
     left: 5,
     shadowOpacity: 5.0,
-  },
-  actionBtn: {
     backgroundColor: "#2CBA8D",
-    textShadowOffset: { width: 5, height: 5 },
-    textShadowRadius: 10,
+  },
+
+  addButton: {
+    position: "absolute",
+    width: 70,
+    height: 85,
+    borderRadius: 35,
+    bottom: 35,
+    zIndex: 10,
+    backgroundColor: "white",
   },
 });
-// import React from "react";
-// import { StyleSheet, Text, View } from "react-native";
-
-// function Nav() {
-//   return (
-//     <View style={style.searchBar}>
-//       <View>
-//         <Text>Hello World</Text>
-//       </View>
-//       <View>
-//         <Text>Hello World</Text>
-//       </View>
-//       <View>
-//         <Text>Hello World</Text>
-//       </View>
-//       <View>
-//         <Text>Hello World</Text>
-//       </View>
-//       <View>
-//         <Text>Hello World</Text>
-//       </View>
-//     </View>
-//   );
-// }
-
-// let style = StyleSheet.create({
-//   searchBar: {
-//     backgroundColor: "#2CBA8D",
-//     width: "100%",
-//     height: 50,
-//     // justifyContent: "center",
-//     alignItems: "center",
-//     position: "absolute",
-//     bottom: 0,
-//   },
-// });
-
-// export default Nav;
